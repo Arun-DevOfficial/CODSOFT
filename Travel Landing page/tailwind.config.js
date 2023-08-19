5be9dc8a3be09e982a20862ec9e2e0417e3012ff
Arun-DevOfficial <darunkumar1204@gmail.com>
2023-08-19 10:04:00 +0530

    First Commit

---
+++ Travel Landing page/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'pop':[" 'Poppins', sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
